5b0a5a8a5093ecd48e715e23182e92572608f104
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-08-12 16:23:01 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className='App'>
       {/* <Context /> */}
-      <CartExercise />
+      {/* <CartExercise /> */}
       {/* <Buttons /> */}
       {/* <FifaTourney /> */}
       {/* <Class /> */}
